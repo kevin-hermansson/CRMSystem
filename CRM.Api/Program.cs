6d160f8fbdb66575fc1ec381a507aec6fc6a03e1
kevin-hermansson <hermanssonkevin@gmail.com>
2026-05-19 21:29:33 +0200

    fixed endpoints

--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -14,12 +14,39 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//GetAll
+app.MapGet("/customers", async (CosmosDbService cosmosDbService) =>
+{
+    var customers = await cosmosDbService.GetCustomersAsync();
+    return Results.Ok(customers);
+});
+//Create
 app.MapPost("/customers", async (Customer customer, CosmosDbService cosmosDbService) =>
 {
     customer.Id = Guid.NewGuid().ToString();
     await cosmosDbService.AddCustomerAsync(customer);
 
     return Results.Ok(customer);
+});
+//Search
+app.MapGet("/customers/search", async (string search, CosmosDbService cosmosDbService) =>
+{
+    var customers = await cosmosDbService.SearchCustomersAsync(search);
+    return Results.Ok(customers);
+});
+//Update
+app.MapPut("/customers/{id}", async (string id, Customer updatedCustomer, CosmosDbService cosmosDbService) =>
+{
+    updatedCustomer.Id = id;
+    await cosmosDbService.UpdateCustomerAsync(updatedCustomer);
+    return Results.Ok(updatedCustomer);
+});
+//Delete
+app.MapDelete("/customers/{id}", async (string id, CosmosDbService cosmosDbService) =>
+{
+    await cosmosDbService.DeleteCustomerAsync(id);
+    return Results.Ok();
 });
 
 app.UseHttpsRedirection();
--- CRM.Api/Services/CosmosDbService.cs
+++ CRM.Api/Services/CosmosDbService.cs
@@ -23,8 +23,53 @@ public class CosmosDbService
         _container = container.Container;
     }
 
+    public async Task<List<Customer>> GetCustomersAsync()
+    {
+        var query = _container.GetItemQueryIterator<Customer>(
+            new QueryDefinition("SELECT * FROM c"));
+
+        List<Customer> customers = new();
+
+        while (query.HasMoreResults)
+        {
+            var response = await query.ReadNextAsync();
+
+            customers.AddRange(response.ToList());
+        }
+
+        return customers;
+    }
     public async Task AddCustomerAsync(Customer customer)
     {
         await _container.CreateItemAsync(customer, new PartitionKey(customer.Id));
+    }
+
+    public async Task<List<Customer>> SearchCustomersAsync(string search)
+    {
+        var query = new QueryDefinition(
+            "SELECT * FROM c WHERE CONTAINS(c.name, @search) OR CONTAINS(c.salesPerson.name, @search)")
+            .WithParameter("@search", search);
+
+        var iterator = _container.GetItemQueryIterator<Customer>(query);
+
+        List<Customer> customers = new();
+
+        while (iterator.HasMoreResults)
+        {
+            var response = await iterator.ReadNextAsync();
+
+            customers.AddRange(response);
+        }
+
+        return customers;
+    }
+
+    public async Task UpdateCustomerAsync(Customer customer)
+    {
+        await _container.UpsertItemAsync(customer, new PartitionKey(customer.Id));
+    }
+    public async Task DeleteCustomerAsync(string id)
+    {
+            await _container.DeleteItemAsync<Customer>(id, new PartitionKey(id));
     }
 }
