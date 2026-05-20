9b69bcd4cd57d2e998bf886f1c0b5a696ecc502f
kevin-hermansson <hermanssonkevin@gmail.com>
2026-05-20 14:37:58 +0200

    Removed secrets from repository

---
+++ CRM.Functions/CustomerNotificationFunction.cs
@@ -0,0 +1,51 @@
+﻿using CRM.Functions.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace CRM.Functions
+{
+    public class CustomerNotificationFunction
+    {
+        private readonly ILogger _logger;
+
+        public CustomerNotificationFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CustomerNotificationFunction>();
+        }
+
+        [Function("CustomerNotificationFunction")]
+        public void Run([CosmosDBTrigger(
+            databaseName: "CRMDatabase",
+            containerName: "Customers",
+            Connection = "CosmosDbConnection",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)] 
+        IReadOnlyList<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                _logger.LogInformation($"Customer changed: {customer.Name}");
+
+                using var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+                {
+                    Credentials = new NetworkCredential(
+                        "4016c71f0f300b",
+                        "efa07ff4c4a2e9"),
+
+                    EnableSsl = true
+                };
+
+                client.Send(
+                    "test@mailtrap.io",
+                    customer.SalesPerson.Email,
+                    "New Customer Assigned",
+                    $"A new customer, {customer.Name}, has been assigned to you.");
+                    
+            }
+        }
+    }
+
+}
---
+++ CRM.Functions/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+namespace CRM.Functions.Models
+{
+    public class Customer
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public SalesPerson SalesPerson { get; set; } = new SalesPerson();
+    }
+}
